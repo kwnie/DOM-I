9bf89545baeee3a862f0ce16e22d75a2ce727f0a
Kelsey Nielsen <kelseywnielsen@gmail.com>
2020-12-02 10:05:16 -0700

    added nav text

--- js/index.js
+++ js/index.js
@@ -38,5 +38,16 @@ const siteContent = {
 };
 
 // Example: Update the img src for the logo
-let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+//let logo = document.getElementById("logo-img");
+//logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+const navMenu = document.querySelectorAll('nav a');
+const navMenu1 = navMenu[0].textContent = siteContent["nav"]["nav-item-1"];
+const navMenu2 = navMenu[1].textContent = siteContent["nav"]["nav-item-2"];
+const navMenu3 = navMenu[2].textContent = siteContent["nav"]["nav-item-3"];
+const navMenu4 = navMenu[3].textContent = siteContent["nav"]["nav-item-4"];
+const navMenu5 = navMenu[4].textContent = siteContent["nav"]["nav-item-5"];
+const navMenu6 = navMenu[5].textContent = siteContent["nav"]["nav-item-6"];
+
+
+
