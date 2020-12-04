b926d569af41f799ce173e9ae5131163e72995f4
Kelsey Nielsen <kelseywnielsen@gmail.com>
2020-12-03 17:16:21 -0700

    added part of for loop for next section

--- js/index.js
+++ js/index.js
@@ -47,16 +47,9 @@ for(i = 0; i < navMenu.length; i++){
   navMenu[i].textContent = siteContent.nav[`nav-item-${i}`]
 };
 
-//navMenu[0].textContent = siteContent["nav"]["nav-item-1"];
-//navMenu[1].textContent = siteContent["nav"]["nav-item-2"];
-//navMenu[2].textContent = siteContent["nav"]["nav-item-3"];
-//navMenu[3].textContent = siteContent["nav"]["nav-item-4"];
-//navMenu[4].textContent = siteContent["nav"]["nav-item-5"];
-//navMenu[5].textContent = siteContent["nav"]["nav-item-6"];
-
 //logo
 const navLogo = document.querySelector('#logo-img');
-navLogo.setAttribute('src', siteContent["nav"]["img-src"]);
+navLogo.setAttribute('src', siteContent.nav["img-src"]);
 
 //heading
 const heading = document.querySelector('h1');
@@ -73,14 +66,22 @@ headingImg.setAttribute('src', siteContent["cta"]["img-src"]);
 
 //small headings
 const smallHeading = document.querySelectorAll('h4');
-smallHeading[0].textContent = siteContent["main-content"]["features-h4"];
-smallHeading[1].textContent = siteContent["main-content"]["about-h4"];
-smallHeading[2].textContent = siteContent["main-content"]["services-h4"];
-smallHeading[3].textContent = siteContent["main-content"]["product-h4"];
-smallHeading[4].textContent = siteContent["main-content"]["vision-h4"];
+for (i = 0; i < smallHeading.length; i++){
+  for (const index in siteContent) {
+    smallHeading[i].textContent = `${siteContent["main-content"][index]}`
+  };
+};
+//smallHeading[0].textContent = siteContent["main-content"]["features-h4"];
+//smallHeading[1].textContent = siteContent["main-content"]["about-h4"];
+//smallHeading[2].textContent = siteContent["main-content"]["services-h4"];
+//smallHeading[3].textContent = siteContent["main-content"]["product-h4"];
+//smallHeading[4].textContent = siteContent["main-content"]["vision-h4"];
 
 //main content 
 const mainContent = document.querySelectorAll('.text-content p');
+//for(i = 0; i < mainContent.length; i++){
+  //mainContent
+//}
 mainContent[0].textContent = siteContent["main-content"]["features-content"];
 mainContent[1].textContent = siteContent["main-content"]["about-content"];
 mainContent[2].textContent = siteContent["main-content"]["services-content"];
